9183273ae22c2a323e9d2d7727b8229cdc1571a4
Oleg Shilov <shilov-1@yandex.ru>
2023-09-08 16:50:34 +0300

    added example of local store;

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,42 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { StoreProvider } from 'true-npm-package-core/store';
 import { Checking, Counter } from 'true-npm-package-core/components';
+
+import { store } from 'store';
+
+import { Wrapper, Counter as ClientCounter } from 'components';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
 
 root.render(
   <StoreProvider>
-    <Checking />
-    <Counter />
+    <Provider store={store}>
+      <Wrapper>
+        <h1>
+          Elements from core
+        </h1>
+
+        <Checking />
+        <Counter />
+      </Wrapper>
+
+      <Wrapper>
+        <h1>
+          Elements from local
+        </h1>
+
+        <ClientCounter />
+      </Wrapper>
+
+      <Wrapper>
+        <h4>
+          They both use identical stores (namespaces, selectors and so on),
+          <br />
+          but work together without any issues or collisions
+        </h4>
+      </Wrapper>
+    </Provider>
   </StoreProvider>
 );
